6cb6974286556ce674dfda9c86b822d9291d55e4
Technokarts@123 <priyanka.nevase@technokarts.com>
2021-09-12 00:52:33 +0530

    Pritesh Commit

---
+++ app/src/main/java/com/example/solutelabs_pritesh/Activity/BookmarkTab.java
@@ -0,0 +1,16 @@
+package com.example.solutelabs_pritesh.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.solutelabs_pritesh.R;
+
+public class BookmarkTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark_tab);
+    }
+}
--- app/src/main/java/com/example/solutelabs_pritesh/Activity/UsersTab.java
+++ app/src/main/java/com/example/solutelabs_pritesh/Activity/UsersTab.java
@@ -1,14 +1,16 @@
-package com.example.solutelabs_pritesh;
+package com.example.solutelabs_pritesh.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.solutelabs_pritesh.R;
+
+public class UsersTab extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_users_tab);
     }
 }
