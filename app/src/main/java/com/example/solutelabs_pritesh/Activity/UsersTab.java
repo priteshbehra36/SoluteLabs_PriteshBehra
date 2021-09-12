fdb43efa64bb3ec72d5498b6a7ab7b3d92bfbcf6
Technokarts@123 <priyanka.nevase@technokarts.com>
2021-09-12 13:13:35 +0530

    Pritesh Commit

--- app/src/main/java/com/example/solutelabs_pritesh/Activity/UsersTab.java
+++ app/src/main/java/com/example/solutelabs_pritesh/Activity/UsersTab.java
@@ -4,13 +4,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.solutelabs_pritesh.Adapter.Adapter;
+import com.example.solutelabs_pritesh.Model.Song;
 import com.example.solutelabs_pritesh.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersTab extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Song> songs;
+    private static String JSON_URL = "https://api.github.com/users?per_page=2&since=1";
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_tab);
+        recyclerView = findViewById(R.id.songsList);
+        songs = new ArrayList<>();
+        extractSongs();
+    }
+
+    private void extractSongs() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject songObject = response.getJSONObject(i);
+
+                        Song song = new Song();
+                        song.setTitle(songObject.getString("node_id").toString());
+                        song.setArtists(songObject.getString("login".toString()));
+                        song.setCoverImage(songObject.getString("avatar_url"));
+                        song.setSongURL(songObject.getString("followers_url"));
+                        songs.add(song);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                adapter = new Adapter(getApplicationContext(),songs);
+                recyclerView.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("tag", "onErrorResponse: " + error.getMessage());
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+
     }
 }
